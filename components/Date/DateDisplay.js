fd74518200fd04beeb8f7731bfe97f0f13928f68
Thai Nguyen <discoverylight@yahoo.com>
2019-06-21 10:47:30 -0700

    added browser's currrent date display

---
+++ components/Date/DateDisplay.js
@@ -0,0 +1,17 @@
+class DateDisplay {
+    constructor(element) {
+        this.element = element;
+
+        this.date = new Date();
+
+        this.days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        
+        this.formatDate = `${this.days[this.date.getDay()]} ${this.date.getMonth() + 1}/${this.date.getDate() + 1}/${this.date.getFullYear() + 1}`
+
+        this.element.textContent = this.formatDate;
+    }
+}
+
+
+const container =  document.querySelector(".date");
+new DateDisplay(container);
