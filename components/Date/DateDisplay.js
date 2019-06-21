bb36fddc1e899acf32ccf7b8612e5f0880bb372a
Thai Nguyen <discoverylight@yahoo.com>
2019-06-21 10:50:40 -0700

    mvp complete + stretch exercise

--- components/Date/DateDisplay.js
+++ components/Date/DateDisplay.js
@@ -1,3 +1,5 @@
+
+
 class DateDisplay {
     constructor(element) {
         this.element = element;
@@ -5,13 +7,11 @@ class DateDisplay {
         this.date = new Date();
 
         this.days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        
+
         this.formatDate = `${this.days[this.date.getDay()]} ${this.date.getMonth() + 1}/${this.date.getDate() + 1}/${this.date.getFullYear() + 1}`
 
         this.element.textContent = this.formatDate;
     }
 }
 
-
-const container =  document.querySelector(".date");
-new DateDisplay(container);
+new DateDisplay(document.querySelector(".date"));
